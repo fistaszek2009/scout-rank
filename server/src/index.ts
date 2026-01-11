3775bb67bbe563a56efec83de667d02160f7e3d1
Pawello09 <kontakt.pb.09@gmail.com>
2026-01-11 19:47:17 +0100

    chore: add cors to server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import cors from 'cors';
 import addV1Route from './routes/api/v1/routes';
 
 const PORT = process.env.PORT ?? 3000;
@@ -8,6 +9,7 @@ const app = express();
 dotenv.config();
 
 app.use(express.json());
+app.use(cors());
 
 addV1Route(app);
 
